8134a47be7feb2bc6aa41670ba8d4bd14f407e9a
Marlene <marlene.dirnberger@edu.fh-joanneum.at>
2020-06-20 17:45:34 +0200

    Create Status Lists 3 Queries: Filtern nach liked, disliked und blocked um 3 Listen zu erstellen.

--- src/main/kotlin/at/fhj/ima/spuddy/repository/UserLikeRepository.kt
+++ src/main/kotlin/at/fhj/ima/spuddy/repository/UserLikeRepository.kt
@@ -7,6 +7,19 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 interface UserLikeRepository: JpaRepository<UserLike, Int> {
-    @Query("FROM UserLike where swipingUser = :swipingUser")
+    @Query("FROM UserLike WHERE swipingUser = :swipingUser")
     fun findLikesBySwipingUser(@Param("swipingUser") swipingUser: User): UserLike
+
+    @Query ("FROM UserLike WHERE swipingUser = :swipingUser AND statusLikes = 'liked'")
+    fun createLikedList(@Param ("swipingUser") swipingUser: User): List<UserLike>
+
+    @Query ("FROM UserLike WHERE swipingUser = :swipingUser AND statusLikes = 'disliked'")
+    fun createDislikedList(@Param ("swipingUser") swipingUser: User): List<UserLike>
+
+    @Query ("FROM UserLike WHERE swipingUser = :swipingUser AND statusLikes = 'blocked'")
+    fun createBlockedList(@Param ("swipingUser") swipingUser: User): List<UserLike>
+
+
+    //@Query("FROM UserLike WHERE swipingUser = :swipingUser AND statusLikes NOT :liked or :blocked or :disliked")
+    //fun
 }
