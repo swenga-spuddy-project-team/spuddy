670589845d124b306c007cf2fbcd362b4bd24015
Marlene <marlene.dirnberger@edu.fh-joanneum.at>
2020-06-20 17:48:16 +0200

    Create List for User District + User Sports

--- src/main/kotlin/at/fhj/ima/spuddy/repository/UserRepository.kt
+++ src/main/kotlin/at/fhj/ima/spuddy/repository/UserRepository.kt
@@ -1,5 +1,7 @@
 package at.fhj.ima.spuddy.repository
 
+import at.fhj.ima.spuddy.entity.District
+import at.fhj.ima.spuddy.entity.Sport
 import at.fhj.ima.spuddy.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
@@ -10,4 +12,10 @@ import org.springframework.stereotype.Repository
 interface UserRepository : JpaRepository<User, Int> {
     @Query("FROM User where username = :username")
     fun findByUsername(@Param("username") username: String): User?
+
+    @Query("FROM User WHERE district = :district")
+    fun createMyDistrictList(@Param("district") district: District?): List<User>
+
+    @Query("FROM User WHERE sport = :sport")
+    fun createMySportList(@Param("sport") sport:Set<Sport>?): List<User>
 }
