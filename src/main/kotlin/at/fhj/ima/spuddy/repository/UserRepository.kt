fc2aa33b10b0f2a78e180defa1837c2abdd5809e
Marlene <marlene.dirnberger@edu.fh-joanneum.at>
2020-06-21 19:58:20 +0200

    Update UserRepository Queries

--- src/main/kotlin/at/fhj/ima/spuddy/repository/UserRepository.kt
+++ src/main/kotlin/at/fhj/ima/spuddy/repository/UserRepository.kt
@@ -13,12 +13,18 @@ interface UserRepository : JpaRepository<User, Int> {
     @Query("from User where username = :username")
     fun findByUsername(@Param("username") username: String): User?
 
-   // @Query("FROM User WHERE district = :district")
-  //  fun createMyDistrictList(@Param("district") district: District?): List<User>
+    @Query("from User where district = :district")
+    fun sharedDistrictAndSports(@Param("district") district: District): List<User>
 
-   // @Query("FROM User WHERE sport = :sport")
-   // fun createMySportList(@Param("sport") sport:Set<Sport>?): List<User>
+    @Query("from User where id = :userId")
+    fun findByUserId(@Param("userId") id: Int): User?
 
-    @Query("from User where district = :district and sport = :sport")
-    fun sharedDistrictAndSports(@Param("district") district: District, @Param("sport") sport:Set<Sport>?): List<User>
+    //@Query("from User where district = :district and sport = :sport")
+    //fun sharedDistrictAndSports(@Param("district") districtId: Int, @Param("sport") sport:Set<Sport>?): List<User>
 }
+
+// @Query("FROM User WHERE district = :district")
+//  fun createMyDistrictList(@Param("district") district: District?): List<User>
+
+// @Query("FROM User WHERE sport = :sport")
+// fun createMySportList(@Param("sport") sport:Set<Sport>?): List<User>
